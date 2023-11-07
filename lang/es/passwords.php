8919581165a46911514141bd8bce8c9eb4b55505
Emanuele-14 <tommasi.emanuelet@libero.it>
2023-11-07 21:53:19 +0100

    set lenguages

---
+++ lang/es/passwords.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Tu contraseña ha sido cambiada.',
+    'sent' => 'Te hemos enviado un correo electrónico con el enlace para restablecer tu contraseña.',
+    'throttled' => 'Espere antes de volver a intentarlo.',
+    'token' => 'QEste token no es válido para restablecer su contraseña.',
+    'user' => "No hay usuarios con esta dirección de correo electrónico.",
+
+
+];
