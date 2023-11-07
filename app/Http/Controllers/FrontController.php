fb9da73a468b9d127f2f4921d54c0d6d53a1ed5a
Emanuele-14 <tommasi.emanuelet@libero.it>
2023-11-07 11:09:12 +0100

    add model & controller

---
+++ app/Http/Controllers/FrontController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Announcement;
+use App\Models\Category;
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class FrontController extends Controller
+{
+    
+    public function homepage() {
+        
+        $announcements = Announcement::where('is_accepted', true)->orderBy('updated_at', 'desc')->get()->take(4);
+        return view('homepage', compact('announcements'));
+    }
+
+    public function categoryShow(Category $category) {
+        $categoriesPaginate = $category->announcements()
+        ->where('is_accepted', true)
+        ->orderBy('updated_at', 'desc')
+        ->paginate(4);
+        return view('detail', compact('category','categoriesPaginate'));
+    }
+
+    public function searchAnnouncements(Request $request){
+        $announcements = Announcement::search($request->searched)->where('is_accepted', true)->orderBy('updated_at', 'desc')->paginate(4);
+        return view('announcements.index', compact('announcements'));
+    }
+}
--- app/Models/Announcement.php
+++ app/Models/Announcement.php
@@ -4,19 +4,37 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Scout\Searchable;
 
 class Announcement extends Model
 {
     use HasFactory;
+    use Searchable;
 
-    protected $fillable = [
+    protected $fillable = 
+    [
         'title',
         'body',
         'price',
+        'category_id'
     ];
     
 
-    public function user(){
+    public function toSearchableArray()
+    {
+        $category = $this->category;
+        $array = 
+        [
+            'id' => $this->id,
+            'title' => $this->title,
+            'body'=> $this->body,
+            'category' => $category
+        ];
+        return $array;
+    }
+
+    public function user()
+    {
         return $this->belongsTo(User::class);
     }
 }
