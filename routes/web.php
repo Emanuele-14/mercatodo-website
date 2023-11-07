906726233ed208c0fb901de289b6ada6fe674a1e
Emanuele-14 <tommasi.emanuelet@libero.it>
2023-11-07 11:19:19 +0100

    route creation

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AnnouncementController;
+use App\Http\Controllers\FrontController;
+use App\Models\Category;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//ROUTE HOMEPAGE
+
+Route::get('/', [FrontController::class, 'homepage'])->name('homepage');
+
+//ROUTE CATEGORIES
+
+Route::get('/categorie/{category}', [FrontController::class,'categoryShow'])->name('category.show');
+
+//ROUTE ANNOUNCEMENTS
+
+
+Route::get('/nuovo/annuncio', [AnnouncementController::class, 'createAnnouncement'])->middleware('auth')->name('announcements.create');
+Route::get('/annunci/{announcement}', [AnnouncementController::class, 'showAnnouncement', ])->name('announcements.show');
+Route::get('/annunci', [AnnouncementController::class, 'index'])->name('announcements.index');
+
+//ROUTE SEARCH ANNOUNCEMENTS
+
+Route::get('/ricerca/annuncio', [FrontController::class, 'searchAnnouncements'])->name('announcements.search');
